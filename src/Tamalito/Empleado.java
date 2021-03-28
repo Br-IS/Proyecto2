d2f81357be9af336a1387e25f32ac11b2bf0c162
velez <velez@192.168.0.2>
2021-03-27 22:34:28 -0500

    Creer las Clase Padre Persona y sus clases hijas Cliente y Empleado. Cada uno con su constructor y sus getters y setters.

---
+++ src/Tamalito/Empleado.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tamalito;
+
+/**
+ *
+ * @author velez
+ */
+public class Empleado {
+    private String sueldo, cod_rol;
+
+    public Empleado(String sueldo, String cod_rol) {
+        this.sueldo = sueldo;
+        this.cod_rol = cod_rol;
+    }
+
+    public String getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(String sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public String getCod_rol() {
+        return cod_rol;
+    }
+
+    public void setCod_rol(String cod_rol) {
+        this.cod_rol = cod_rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "sueldo=" + sueldo + ", cod_rol=" + cod_rol + '}';
+    }
+}
