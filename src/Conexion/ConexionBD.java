2f794e8b028de3b0695c01b7d82bd1e8b99f6fa4
Boris <Boris@DESKTOP-709JLTS>
2021-04-07 18:30:13 -0500

    VISTA GRAFICA JUICIOS Y AUDIENCIAS

---
+++ src/Conexion/ConexionBD.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+/**
+ *
+ * @author Boris
+ */
+public class ConexionBD {
+    
+    
+    /*
+    String BD = "jdbc:postgresql://localhost:5432/Interfaces";//ruta donde se encuentra la tabla
+    String usuario = "postgres";
+    String contrasena = "Borisq19";
+    Connection con = null;
+
+    public ConexionBD() {
+        try {
+            con = DriverManager.getConnection(BD, usuario, contrasena);
+            Statement st = con.createStatement();
+            JOptionPane.showMessageDialog(null, "conectado correctamente");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de conexion" + e);
+        }
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
+    */
+}
